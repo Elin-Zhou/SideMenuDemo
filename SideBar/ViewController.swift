8c58406079a35cb8719e36523ffb797108ef129d
allenzhou <ansession.com@gmail.com>
2015-04-18 23:26:48 +0800

    Initial Commit

---
+++ SideBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SideBar
+//
+//  Created by Allen on 15/4/18.
+//  Copyright (c) 2015年 Elin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
